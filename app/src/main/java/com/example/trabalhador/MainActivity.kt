4acc00f37e7cbc90a706e27e40eb78604c612a3a
Avila-Fe <whiteavila6@gmail.com>
2023-05-15 09:07:55 -0300

    Base layout complete

--- app/src/main/java/com/example/trabalhador/MainActivity.kt
+++ app/src/main/java/com/example/trabalhador/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.trabalhador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.trabalhador.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavigation()
+    }
+
+    private fun initNavigation(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
